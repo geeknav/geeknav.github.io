c057c027fe8fa3398241d5288674000731bd5c0a
geeknav <944467720@qq.com>
2017-04-02 17:50:00 +0800

    Update geeknav-data.js

--- geeknav-data.js
+++ geeknav-data.js
@@ -1,143 +1,30 @@
 var geeknavData = {
   site:{
-    'Java':[
+    '综合':[
       {
-        'img':'https://static.segmentfault.com/v-58df7e8a/global/img/favicon.ico',
-        'title':'LeanCloud',
-        'link':'http://www.baidu.com',
-        'detail':'行在虚拟机上的容器可以直接使用 IaaS 平台的 SDN 网络，从而极大提升容器应用的网络性能，并显著降低配置与维护的操作难'
+        'img':'http://img.zcool.cn/community/011b4e569615756ac725af2358c9f2.png',
+        'title':'Google',
+        'link':'https://www.google.com',
+        'detail':'全球最大的搜索引擎'
       },
       {
-        'img':'https://static.segmentfault.com/v-58df7e8a/global/img/favicon.ico',
-        'title':'LeanCloud',
-        'detail':'行在虚拟机上的容器可以直接使用 IaaS 平台的 SDN 网络，从而极大提升容器应用的网络性能，并显著降低配置与维护的操作难'
+        'img':'https://cdn.sstatic.net/Sites/stackoverflow/img/apple-touch-icon.png',
+        'title':'StackOverflow',
+        'link':'http://stackoverflow.com/',
+        'detail':'一个与程序相关的IT技术问答网站'
       },
       {
-        'img':'https://static.segmentfault.com/v-58df7e8a/global/img/favicon.ico',
-        'title':'LeanCloud',
-        'detail':'行在虚拟机上的容器可以直接使用 IaaS 平台的 SDN 网络，从而极大提升容器应用的网络性能，并显著降低配置与维护的操作难'
+        'img':'https://static.segmentfault.com/v-58dfce02/global/img/touch-icon.png',
+        'title':'SegmentFault',
+        'link':'https://segmentfault.com/',
+        'detail':'SegmentFault是中国领先的开发者社区。旨在为中文开发者提供一个纯粹、高质的技术交流平台'
       },
       {
-        'img':'https://static.segmentfault.com/v-58df7e8a/global/img/favicon.ico',
-        'title':'LeanCloud',
-        'link':'http://www.baidu.com',
-        'detail':'行在虚拟机上的容器可以直接使用 IaaS 平台的 SDN 网络，从而极大提升容器应用的网络性能，并显著降低配置与维护的操作难'
+        'img':'http://c.csdnimg.cn/public/favicon.ico',
+        'title':'CSDN',
+        'link':'http://www.csdn.net/',
+        'detail':'全球最大的中文IT社区'
       },
-      {
-        'img':'https://static.segmentfault.com/v-58df7e8a/global/img/favicon.ico',
-        'title':'LeanCloud',
-        'detail':'行在虚拟机上的容器可以直接使用 IaaS 平台的 SDN 网络，从而极大提升容器应用的网络性能，并显著降低配置与维护的操作难'
-      },
-      {
-        'img':'https://static.segmentfault.com/v-58df7e8a/global/img/favicon.ico',
-        'title':'LeanCloud',
-        'detail':'行在虚拟机上的容器可以直接使用 IaaS 平台的 SDN 网络，从而极大提升容器应用的网络性能，并显著降低配置与维护的操作难'
-      },
-      {
-        'img':'https://static.segmentfault.com/v-58df7e8a/global/img/favicon.ico',
-        'title':'LeanCloud',
-        'link':'http://www.baidu.com',
-        'detail':'行在虚拟机上的容器可以直接使用 IaaS 平台的 SDN 网络，从而极大提升容器应用的网络性能，并显著降低配置与维护的操作难'
-      },
-      {
-        'img':'https://static.segmentfault.com/v-58df7e8a/global/img/favicon.ico',
-        'title':'LeanCloud',
-        'detail':'行在虚拟机上的容器可以直接使用 IaaS 平台的 SDN 网络，从而极大提升容器应用的网络性能，并显著降低配置与维护的操作难'
-      },
-      {
-        'img':'https://static.segmentfault.com/v-58df7e8a/global/img/favicon.ico',
-        'title':'LeanCloud',
-        'detail':'行在虚拟机上的容器可以直接使用 IaaS 平台的 SDN 网络，从而极大提升容器应用的网络性能，并显著降低配置与维护的操作难'
-      },{
-        'img':'https://static.segmentfault.com/v-58df7e8a/global/img/favicon.ico',
-        'title':'LeanCloud',
-        'link':'http://www.baidu.com',
-        'detail':'行在虚拟机上的容器可以直接使用 IaaS 平台的 SDN 网络，从而极大提升容器应用的网络性能，并显著降低配置与维护的操作难'
-      },
-      {
-        'img':'https://static.segmentfault.com/v-58df7e8a/global/img/favicon.ico',
-        'title':'LeanCloud',
-        'detail':'行在虚拟机上的容器可以直接使用 IaaS 平台的 SDN 网络，从而极大提升容器应用的网络性能，并显著降低配置与维护的操作难'
-      },
-      {
-        'img':'https://static.segmentfault.com/v-58df7e8a/global/img/favicon.ico',
-        'title':'LeanCloud',
-        'detail':'行在虚拟机上的容器可以直接使用 IaaS 平台的 SDN 网络，从而极大提升容器应用的网络性能，并显著降低配置与维护的操作难'
-      },{
-        'img':'https://static.segmentfault.com/v-58df7e8a/global/img/favicon.ico',
-        'title':'LeanCloud',
-        'link':'http://www.baidu.com',
-        'detail':'行在虚拟机上的容器可以直接使用 IaaS 平台的 SDN 网络，从而极大提升容器应用的网络性能，并显著降低配置与维护的操作难'
-      },
-      {
-        'img':'https://static.segmentfault.com/v-58df7e8a/global/img/favicon.ico',
-        'title':'LeanCloud',
-        'detail':'行在虚拟机上的容器可以直接使用 IaaS 平台的 SDN 网络，从而极大提升容器应用的网络性能，并显著降低配置与维护的操作难'
-      },
-      {
-        'img':'https://static.segmentfault.com/v-58df7e8a/global/img/favicon.ico',
-        'title':'LeanCloud',
-        'detail':'行在虚拟机上的容器可以直接使用 IaaS 平台的 SDN 网络，从而极大提升容器应用的网络性能，并显著降低配置与维护的操作难'
-      },{
-        'img':'https://static.segmentfault.com/v-58df7e8a/global/img/favicon.ico',
-        'title':'LeanCloud',
-        'link':'http://www.baidu.com',
-        'detail':'行在虚拟机上的容器可以直接使用 IaaS 平台的 SDN 网络，从而极大提升容器应用的网络性能，并显著降低配置与维护的操作难'
-      },
-      {
-        'img':'https://static.segmentfault.com/v-58df7e8a/global/img/favicon.ico',
-        'title':'LeanCloud',
-        'detail':'行在虚拟机上的容器可以直接使用 IaaS 平台的 SDN 网络，从而极大提升容器应用的网络性能，并显著降低配置与维护的操作难'
-      },
-      {
-        'img':'https://static.segmentfault.com/v-58df7e8a/global/img/favicon.ico',
-        'title':'LeanCloud',
-        'detail':'行在虚拟机上的容器可以直接使用 IaaS 平台的 SDN 网络，从而极大提升容器应用的网络性能，并显著降低配置与维护的操作难'
-      },{
-        'img':'https://static.segmentfault.com/v-58df7e8a/global/img/favicon.ico',
-        'title':'LeanCloud',
-        'link':'http://www.baidu.com',
-        'detail':'行在虚拟机上的容器可以直接使用 IaaS 平台的 SDN 网络，从而极大提升容器应用的网络性能，并显著降低配置与维护的操作难'
-      },
-      {
-        'img':'https://static.segmentfault.com/v-58df7e8a/global/img/favicon.ico',
-        'title':'LeanCloud',
-        'detail':'行在虚拟机上的容器可以直接使用 IaaS 平台的 SDN 网络，从而极大提升容器应用的网络性能，并显著降低配置与维护的操作难'
-      },
-      {
-        'img':'https://static.segmentfault.com/v-58df7e8a/global/img/favicon.ico',
-        'title':'LeanCloud',
-        'detail':'行在虚拟机上的容器可以直接使用 IaaS 平台的 SDN 网络，从而极大提升容器应用的网络性能，并显著降低配置与维护的操作难'
-      }
-    ],
-    'IOS':[
-      {
-        'img':'https://static.segmentfault.com/v-58df7e8a/global/img/favicon.ico',
-        'title':'LeanCloud',
-        'detail':'行在虚拟机上的容器可以直接使用 IaaS 平台的 SDN 网络，从而极大提升容器应用的网络性能，并显著降低配置与维护的操作难'
-      },
-      {
-        'img':'https://static.segmentfault.com/v-58df7e8a/global/img/favicon.ico',
-        'title':'LeanCloud',
-        'detail':'行在虚拟机上的容器可以直接使用 IaaS 平台的 SDN 网络，从而极大提升容器应用的网络性能，并显著降低配置与维护的操作难'
-      },
-      {
-        'img':'https://static.segmentfault.com/v-58df7e8a/global/img/favicon.ico',
-        'title':'LeanCloud',
-        'detail':'行在虚拟机上的容器可以直接使用 IaaS 平台的 SDN 网络，从而极大提升容器应用的网络性能，并显著降低配置与维护的操作难'
-      }
-    ],
-    'Android':[
-      {
-        'img':'https://static.segmentfault.com/v-58df7e8a/global/img/favicon.ico',
-        'title':'LeanCloud',
-        'detail':'行在虚拟机上的容器可以直接使用 IaaS 平台的 SDN 网络，从而极大提升容器应用的网络性能，并显著降低配置与维护的操作难'
-      },
-      {
-        'img':'https://static.segmentfault.com/v-58df7e8a/global/img/favicon.ico',
-        'title':'LeanCloud',
-        'detail':'行在虚拟机上的容器可以直接使用 IaaS 平台的 SDN 网络，从而极大提升容器应用的网络性能，并显著降低配置与维护的操作难'
-      },
-    ],
+    ]
   }
 }
